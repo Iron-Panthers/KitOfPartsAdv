3f7b39acdb446a8fad45ee030223cd484d481fd1
Isabel Dominik <icd107@live.com>
2016-11-18 16:46:35 -0800

    Added joystick code

--- KidzBob/src/org/usfirst/frc/team5026/robot/subsystems/Drive.java
+++ KidzBob/src/org/usfirst/frc/team5026/robot/subsystems/Drive.java
@@ -3,14 +3,17 @@ package org.usfirst.frc.team5026.robot.subsystems;
 import org.usfirst.frc.team5026.robot.Hardware;
 import org.usfirst.frc.team5026.robot.Robot;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drive extends Subsystem {
 	
 	private Hardware hardware;
+	private RobotDrive drive;
 	
 	public Drive() {
 		hardware = Robot.hardware;
+		drive = new RobotDrive(hardware.leftDriveMotors, hardware.rightDriveMotors);
 	}
 
 	@Override
@@ -22,6 +25,10 @@ public class Drive extends Subsystem {
 	public void setMotors(int speed) {
 		hardware.leftDriveMotors.set(speed);
 		hardware.rightDriveMotors.set(speed);
+	}
+	
+	public void driveWithJoystick() {
+		drive.arcadeDrive(hardware.roboJoystick);
 	}
 	
 	public void stopMotors() {
